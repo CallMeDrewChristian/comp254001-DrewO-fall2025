4b1517a685a8b8e53763749840bdbbeb432fde24
CallMeDrewChristian <72636893+CallMeDrewChristian@users.noreply.github.com>
2025-10-10 18:16:21 -0400

    Exercise1 & 2 Done!

---
+++ DrewO_COMP254Lab3/src/Exercise1.java
@@ -0,0 +1,33 @@
+public class Exercise1 {
+    public static int multiplyResult(int m, int n) {
+        if (n == 0) {
+            return 0;
+        }
+        else {
+            return multiplyResult(m, n - 1) + m;
+        }
+    }
+    public static void main(String[] args) {
+        int[][] positiveInt = {
+                {6, 7},
+                {8,8},
+                {20, 50},
+                {100, 10},
+                {12, 0},
+                {400, 400},
+                {1, 1}
+        };
+        for (int[] test : positiveInt) {
+            int m = test[0];
+            int n = test[1];
+            int result = multiplyResult(m, n);
+            int expected = m * n;
+            if (result == expected) {
+                System.out.println("Success! " + test[0] + " x " + test[1] + " = " + result);
+            }
+            else {
+                System.out.println("ERROR: " + test[0] + " x " + test[1] + " = " + result);
+            }
+        }
+    }
+}
---
+++ DrewO_COMP254Lab3/src/Exercise2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+
+
+public class Exercise2 {
+
+    public static boolean checkPalindrome(String s) {
+        if (s.length() <= 1) {
+            return true;
+        }
+        if (s.charAt(0) != s.charAt(s.length() - 1)) {
+            return false;
+        }
+        return checkPalindrome(s.substring(1, s.length() - 1));
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your word: ");
+        String word = scanner.nextLine();
+        word = word.toLowerCase();
+        if (checkPalindrome(word)) {
+            System.out.println(word + " is a palindrome");
+        }
+        else {
+            System.out.println(word + " is not a palindrome");
+        }
+    }
+}
